88c4cbd7a47bc9b50f177a2477cd83cd2a4b7747
yaw oppong <opp.mic@gmail.com>
2024-02-24 15:33:27 +0100

    dark theme

--- src/components/AppContextProvider.jsx
+++ src/components/AppContextProvider.jsx
@@ -5,16 +5,16 @@ export const useAppContext = () => useContext(AppContext);
 const getInitialMode = () => {
   const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
   const storeMode = localStorage.getItem('darkTheme');
+  console.log(typeof storeMode);
   if (storeMode === null) {
 
     return prefersDarkMode;
   }
-  return storeMode === true;
+  return storeMode === 'true';
 }
 const AppContextProvider = ({ children }) => {
   const [darkTheme, setDarkTheme] = useState(getInitialMode())
   const [searchCriteria, setSearchCriteria] = useState('cars');
-
 
   const toggleDarkTheme = () => {
     const newTheme = !darkTheme;
@@ -22,7 +22,7 @@ const AppContextProvider = ({ children }) => {
     localStorage.setItem('darkTheme', newTheme)
   }
 
-
+  console.log(getInitialMode());
 
   useEffect(() => {
     document.body.classList.toggle('dark-theme', darkTheme);
